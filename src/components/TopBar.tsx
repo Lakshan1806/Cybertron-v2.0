d06ddbce0886b3f3cffeb5626400ae435d35edd9
Lakshan1806 <lakshanuthayan@gmail.com>
2025-09-06 21:08:54 +0530

    feat: Replace NavBar with TopBar and Sidebar components
    Removed the NavBar component and added new TopBar and Sidebar components. TopBar includes navigation and authentication links with updated styling. Sidebar component is added as a placeholder.

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+function TopBar() {
+  const navItems = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/service", label: "Service" },
+    { path: "/contact", label: "Contact" },
+  ];
+
+  const authLinks = [
+    { path: "/signup", label: "Sign Up" },
+    { path: "/signin", label: "Sign In" },
+  ];
+
+  return (
+    <div className="absolute top-0 flex h-[80px] w-full items-center bg-white/5 px-2 backdrop-blur-md">
+      <div className="flex w-1/3 justify-center gap-5">
+        {navItems.map((item) => (
+          <Link
+            href={item.path}
+            key={item.path}
+            className="text-grad-stroke text-[20px] font-[500]"
+            data-text={item.label}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </div>
+      <div className="flex w-1/3 justify-end gap-5">
+        {authLinks.map((item) => (
+          <Link
+            href={item.path}
+            key={item.path}
+            className="button-wrapper text-[16px]"
+            data-text={item.label}
+          >
+            <div className="button-primary">{item.label}</div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
